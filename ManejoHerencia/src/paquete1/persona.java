763ebadd1b2a2944b2d00d976ac4f2d0ca5fc7a5
Amonint <payala909@gmail.com>
2022-08-03 22:43:17 -0500

    final

--- ManejoHerencia/src/paquete1/persona.java
+++ ManejoHerencia/src/paquete1/persona.java
@@ -1,7 +1,48 @@
 package paquete1;
 
-public class Persona{
-    protected String nombre;
-    protected String apellidos;
-    protected String username;
+public class persona{
+    private String nombre;
+    private String apellido;
+    private String userName;
+
+    public persona(String a, String b, String c) {
+        nombre = a;
+        apellido = b;
+        userName = c;
+    }
+
+    public void establecerNombre(String a) {
+        nombre = a;
+    }
+
+    public void establecerApellido(String a) {
+        apellido = a;
+    }
+
+    public void establecerUserName(String a) {
+        userName = a;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerApellido() {
+        return apellido;
+    }
+
+    public String obtenerUserName() {
+        return userName;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("\t>> Nombre: %s\n"
+                + "\t>> Apellido: %s\n"
+                + "\t>> UserName: %s\n",
+                 nombre,
+                 apellido,
+                 userName);
+        return cadena;
+    }
 }
